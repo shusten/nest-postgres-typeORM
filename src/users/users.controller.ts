132058455f3587b96305427bed9e8d66f4ebfe5d
Luis Villas-Boas <shusten@hotmail.com>
2024-05-21 20:04:49 -0300

    Add get and post in controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { Response } from 'express';
+import { UsersDto } from './users.dto.ts';
 
 @Controller('users')
 export class UsersController {
     constructor( 
         private readonly usersService: UsersService
     ) {}
+
+    @Get()
+    findAllUsers(@Res() response: Response) {
+        return response.status(200).json(this.usersService.findAllUsers());
+    }
+
+    @Post()
+    async createUser(@Res() response: Response, @Body() userDTO: UsersDto) {
+       const userCreated = await this.usersService.createUser(userDTO);
+        return response.status(201).json(userCreated);
+    }
 }
