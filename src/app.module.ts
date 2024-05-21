dab67f030648e148a1793da6703180ccc43003e1
Luis Villas-Boas <shusten@hotmail.com>
2024-05-21 08:19:24 -0300

    git add lib typeorm and configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_DATABASE,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      synchronize: true,
+      logging: false,
+      migrations: [__dirname + '/database/migrations/*{.js,.ts'],
+      entities: [__dirname + '/**/*.entity{.js,.ts'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
